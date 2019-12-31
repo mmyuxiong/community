ece1d4e0a989e2f3079aa90288dabb0dfeccd383
grady <grady_lee@163.com>
2019-12-31 18:04:34 +0800

    2019.12.31

--- src/main/java/life/majang/community/community/controller/indexController.java
+++ src/main/java/life/majang/community/community/controller/indexController.java
@@ -1,13 +1,45 @@
 package life.majang.community.community.controller;
 
+import life.majang.community.community.Dto.PaginationDTO;
+import life.majang.community.community.Dto.QuestionDTO;
+import life.majang.community.community.Mapper.UserMapper;
+import life.majang.community.community.Service.QuestionService;
+import life.majang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @Controller
 public class indexController {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request, Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size
+    ){
+        Cookie[] cookies=request.getCookies();
+        for (Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        PaginationDTO pagination=questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 
